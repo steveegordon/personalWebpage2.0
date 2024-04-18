c8f9877e185ee3e808dd7d67309a546eb23513c1
Steven Gordon <tdtsgordon@gmail.com>
2024-04-18 16:31:26 -0700

    add hex components with grid layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import _hexTile from './components/_hex-tile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p>Some temp content in here</p>
       </header>
+      <div className="grid-container">
+        <_hexTile></_hexTile>
+        <_hexTile></_hexTile>
+        <_hexTile></_hexTile>
+        <_hexTile></_hexTile>
+        <_hexTile></_hexTile>
+      </div>
     </div>
   );
 }
---
+++ src/components/_hex-tile.js
@@ -0,0 +1,11 @@
+import './_hex-tile.css';
+
+function _hexTile() {
+    return (
+        <div className="hexTile">
+
+        </div>
+    );
+}
+
+export default _hexTile;
